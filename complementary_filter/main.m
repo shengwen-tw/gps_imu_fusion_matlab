ef8199e0f0777e4332e272904323586fa0e55ea4
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-10-13 13:08:43 +0800

    implement attitude complementary filter

---
+++ complementary_filter/main.m
@@ -0,0 +1,136 @@
+csv = csvread("gps_imu_compass_barometer_log1.csv");
+
+%ms
+timestamp_ms = csv(:, 1);
+%m/s^2
+accel_lpf_x = csv(:, 2);
+accel_lpf_y = csv(:, 3);
+accel_lpf_z = csv(:, 4);
+%rad/s
+gyro_raw_x = csv(:, 5);
+gyro_raw_y = csv(:, 6);
+gyro_raw_z = csv(:, 7);
+%uT
+mag_raw_x = csv(:, 8);
+mag_raw_y = csv(:, 9);
+mag_raw_z = csv(:, 10);
+%degree
+longitude = csv(:, 11);
+latitude = csv(:, 12);
+%m
+gps_height_msl = csv(:, 13);
+%m/s
+gps_ned_vx = csv(:, 14);
+gps_ned_vy = csv(:, 15);
+gps_ned_vz = csv(:, 16);
+%m
+barometer_height = csv(:, 17);
+%m/s
+barometer_vz = csv(:, 18);
+
+timestamp_s = timestamp_ms .* 0.001;
+data_num = size(timestamp_ms);
+attitude_estimator = attitude;
+dt = 0.01; %100Hz, 0.01s
+
+for i = 1: data_num
+    attitude_estimator.complementary_filter(accel_lpf_x(i), accel_lpf_y(i), accel_lpf_z(i), ...
+                                            gyro_raw_x(i), gyro_raw_y(i), gyro_raw_z(i), ...
+                                            mag_raw_x(i), mag_raw_y(i), mag_raw_z(i), dt);
+end
+
+%%%%%%%%
+% Plot %
+%%%%%%%%
+
+%accelerometer
+figure('Name', 'accelerometer');
+subplot (3, 1, 1);
+plot(timestamp_s, accel_lpf_x);
+title('accelerometer');
+xlabel('time [s]');
+ylabel('ax [m/s^2]');
+subplot (3, 1, 2);
+plot(timestamp_s, accel_lpf_y);
+xlabel('time [s]');
+ylabel('ay [m/s^2]');
+subplot (3, 1, 3);
+plot(timestamp_s, accel_lpf_z);
+xlabel('time [s]');
+ylabel('az [m/s^2]');
+
+%gyroscope
+figure('Name', 'gyroscope');
+subplot (3, 1, 1);
+plot(timestamp_s, gyro_raw_x);
+title('gyroscope');
+xlabel('time [s]');
+ylabel('wx [rad/s]');
+subplot (3, 1, 2);
+plot(timestamp_s, gyro_raw_y);
+xlabel('time [s]');
+ylabel('wy [rad/s]');
+subplot (3, 1, 3);
+plot(timestamp_s, gyro_raw_z);
+xlabel('time [s]');
+ylabel('wz [rad/s]');
+
+%magnatometer
+figure('Name', 'magnetometer');
+subplot (3, 1, 1);
+plot(timestamp_s, mag_raw_x);
+title('magnetometer');
+xlabel('time [s]');
+ylabel('mx [uT]');
+subplot (3, 1, 2);
+plot(timestamp_s, mag_raw_z);
+xlabel('time [s]');
+ylabel('my [uT]');
+subplot (3, 1, 3);
+plot(timestamp_s, mag_raw_z);
+xlabel('time [s]');
+ylabel('mx [uT]');
+
+%gps position
+figure('Name', 'gps position (wgs84)');
+subplot (3, 1, 1);
+plot(timestamp_s, longitude);
+title('gps position (wgs84)');
+xlabel('time [s]');
+ylabel('longitude [deg]');
+subplot (3, 1, 2);
+plot(timestamp_s, latitude);
+xlabel('time [s]');
+ylabel('latitude [deg]');
+subplot (3, 1, 3);
+plot(timestamp_s, gps_height_msl);
+xlabel('time [s]');
+ylabel('height MSL [m]');
+
+%gps velocity
+figure('Name', 'gps velocity');
+subplot (3, 1, 1);
+plot(timestamp_s, gps_ned_vx);
+title('gps velocity');
+xlabel('time [s]');
+ylabel('vx [m/s]');
+subplot (3, 1, 2);
+plot(timestamp_s, gps_ned_vy);
+xlabel('time [s]');
+ylabel('my [m/s]');
+subplot (3, 1, 3);
+plot(timestamp_s, gps_ned_vz);
+xlabel('time [s]');
+ylabel('vz [m/s]');
+
+%barometer
+figure('Name', 'barometer');
+subplot (2, 1, 1);
+plot(timestamp_s, barometer_height);
+title('barometer');
+xlabel('time [s]');
+ylabel('z [m]');
+subplot (2, 1, 2);
+plot(timestamp_s, barometer_vz);
+xlabel('time [s]');
+ylabel('vz [m/s]');
