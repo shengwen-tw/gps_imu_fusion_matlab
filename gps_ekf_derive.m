5e4ae5b11b42da3f741a08aceabf11b2b0063294
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-11-06 15:01:18 +0800

    rewrite ekf deriviation

--- gps_ekf_derive.m
+++ gps_ekf_derive.m
@@ -1,82 +1,24 @@
 pkg load symbolic
 
-%===========================================%
-% define math symbolic derivation variables %
-%===========================================%
+syms q0 q1 q2 q3
 
-syms dt
-syms ax_i ay_i az_i
-syms px_last py_last pz_last vx_last vy_last vz_last
-syms px_gps py_gps pz_gps vx_gps vy_gps vz_gps
-syms p11 p22 p33 p44 p55 p66
-syms q11 q22 q33 q44 q55 q66
-syms r11 r22 r33 r44 r55 r66
+X_delta_x = ...
+[[1 0 0 0 0 0 0     0     0     0];
+ [0 1 0 0 0 0 0     0     0     0];
+ [0 0 1 0 0 0 0     0     0     0];
+ [0 0 0 1 0 0 0     0     0     0];
+ [0 0 0 0 1 0 0     0     0     0];
+ [0 0 0 0 0 1 0     0     0     0];
+ [0 0 0 0 0 0 0 -q1/2 -q2/2 -q3/2];
+ [0 0 0 0 0 0 0  q0/2 -q3/2  q2/2];
+ [0 0 0 0 0 0 0  q3/2  q0/2 -q1/2];
+ [0 0 0 0 0 0 0 -q3/2  q1/2  q0/2]];
 
-x_last = [px_last; py_last; pz_last; vx_last; vy_last; vz_last];
-u = [ax_i; ay_i; az_i];
-z = [px_gps; py_gps; pz_gps; vx_gps; vy_gps; vz_gps];
+H_accel = ...
+[[0 0 0 0 0 0 -2*q2  2*q3 -2*q0 2*q1];
+ [0 0 0 0 0 0  2*q1  2*q0  2*q3 2*q2];
+ [0 0 0 0 0 0  2*q0 -2*q1 -2*q2 2*q3]];
 
-%=====================================%
-% define symbolic derivation matrices %
-%=====================================%
+H = H_accel * X_delta_x
 
-P_last = [p11  0   0   0   0   0;
-           0  p22  0   0   0   0;
-           0   0  p33  0   0   0;
-           0   0   0  p44  0   0;
-           0   0   0   0  p55  0;
-           0   0   0   0   0  p66];
 
-Q = [q11   0   0   0   0   0;
-       0 q22   0   0   0   0;
-       0   0  q33  0   0   0;
-       0   0   0  q44  0   0;
-       0   0   0   0  q55  0;
-       0   0   0   0   0  q66];
-
-R = [r11   0   0   0   0   0;
-       0 r22   0   0   0   0;
-       0   0  r33  0   0   0;
-       0   0   0  r44  0   0;
-       0   0   0   0  r55  0;
-       0   0   0   0   0  r66];
-
-A = [0 0 0 1 0 0;
-     0 0 0 0 1 0;
-     0 0 0 0 0 1;
-     0 0 0 0 0 0;
-     0 0 0 0 0 0;
-     0 0 0 0 0 0];
-
-B = [0 0 0;
-     0 0 0;
-     0 0 0
-     1 0 0
-     0 1 0
-     0 0 1];
-
-H = [1 0 0 0 0 0;
-     0 1 0 0 0 0;
-     0 0 1 0 0 0;
-     0 0 0 0 0 0;
-     0 0 0 0 0 0;
-     0 0 0 0 0 0];
-
-I = eye(6);
-
-%===========================%
-% start symbolic derivation %
-%===========================%
-
-x_dot = A*x_last + B*u
-x_priori = x_last + (dt .* x_dot)
-
-P_dot = A*P_last + (P_last*transpose(A)) + Q
-P_priori = P_last + (dt .* P_dot)
-
-PHt = P_priori*H;
-HPHt_R = H*P_priori*transpose(H) + R;
-K = PHt*inv(HPHt_R)
-
-x_posteriori = x_priori + K*(z - H*x_priori)
-P_posteriori = (I - K*H)*P_priori
