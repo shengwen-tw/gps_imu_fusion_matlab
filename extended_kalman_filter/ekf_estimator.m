1c184e58438dea7b9b25682f76d1cbce5587240a
Shengwen Cheng <c.shengwen.tw@gmail.com>
2020-11-16 16:27:31 +0800

    initial import of ekf estimator

---
+++ extended_kalman_filter/ekf_estimator.m
@@ -0,0 +1,226 @@
+classdef ekf_estimator
+    properties
+        inclination_angle = 0;
+        q_inclination = [1; 0; 0; 0];
+        
+        q_last = [1; 0; 0; 0]
+        q_estimate = [1; 0; 0; 0]
+        
+        R = [1 0 0;
+             0 1 0;
+             0 0 1];
+        
+        roll = 0
+        pitch = 0
+        yaw = 0
+    end
+        
+    methods
+        function q_out = quaternion_mult(obj, q1, q2)
+            q_out = [q1(1)*q2(1) - q1(2)*q2(2) - q1(3)*q2(3) - q1(4)*q2(4);
+                     q1(1)*q2(2) + q1(2)*q2(1) + q1(3)*q2(4) - q1(4)*q2(3);
+                     q1(1)*q2(3) - q1(2)*q2(4) + q1(3)*q2(1) + q1(4)*q2(2);
+                     q1(1)*q2(4) + q1(2)*q2(3) - q1(3)*q2(2) + q1(4)*q2(1)];
+        end
+        
+        function q_out = quaternion_conj(obj, q)
+            q_out = [+q(1);
+                     -q(2);
+                     -q(3);
+                     -q(4)];
+        end
+        
+        function q_out = quat_normalize(obj, q)
+            div_q_norm = 1 / norm(q);
+            q_out = [q(1) * div_q_norm;
+                     q(2) * div_q_norm;
+                     q(3) * div_q_norm;
+                     q(4) * div_q_norm];
+        end
+        
+        function q_out = lerp(obj, q1, q2, alpha)
+            q_out = [(1.0 - alpha) * q1(1) + (alpha * q2(1));
+                     (1.0 - alpha) * q1(2) + (alpha * q2(2));
+                     (1.0 - alpha) * q1(3) + (alpha * q2(3));
+                     (1.0 - alpha) * q1(4) + (alpha * q2(4))];
+        end
+        
+        function q = convert_gravity_to_quat(obj, a)
+            if a(3) >= 0.0
+                sqrt_tmp = 1.0 / sqrt(2.0 * (a(3) + 1));
+                q(1) = sqrt(0.5 * (a(3) + 1.0));
+                q(2) = -a(2) * sqrt_tmp;
+                q(3) = +a(1) * sqrt_tmp;
+                q(4) = 0.0;
+            else
+                sqrt_tmp = 1.0 / sqrt(2.0 * (1.0 - a(3)));
+                q(1) = -a(2) * sqrt_tmp;
+                q(2) = sqrt((1.0 - a(3)) * 0.5);
+                q(3) = 0.0;
+                q(4) = a(1) * sqrt_tmp;
+            end
+        end
+        
+        function q = convert_magnetic_field_to_quat(obj, l)
+            gamma = l(1)*l(1) + l(2)*l(2);
+            sqrt_gamma = sqrt(gamma);
+            sqrt_2gamma = sqrt(2.0 * gamma);
+            sqrt_2 = sqrt(2.0);
+
+            if l(1) >= 0.0
+                sqrt_tmp = sqrt(gamma + l(1)*sqrt_gamma);
+                q(1)= sqrt_tmp / sqrt_2gamma;
+                q(2) = 0.0;
+                q(3) = 0.0;
+                q(4) = l(2) / (sqrt_2 * sqrt_tmp);
+            else
+                sqrt_tmp = sqrt(gamma - l(1)*sqrt_gamma);
+                q(1) = l(2) / (sqrt_2 * sqrt_tmp);
+                q(2) = 0.0;
+                q(3) = 0.0;
+                q(4) = sqrt_tmp / sqrt_2gamma;
+            end
+        end
+        
+        function ret_obj = set_inclination_angle(obj, angle_degree)
+            obj.q_inclination = [cos(deg2rad(angle_degree * 0.5));
+                                 0;
+                                 0;
+                                 sin(deg2rad(angle_degree * 0.5))];
+            ret_obj = obj;
+        end
+        
+        function R = prepare_body_to_earth_rotation_matrix(obj, q)
+            q0q0 = q(1) * q(1);
+            q1q1 = q(2) * q(2);
+            q2q2 = q(3) * q(3);
+            q3q3 = q(4) * q(4);
+            q0q3 = q(1) * q(4);
+            q1q2 = q(2) * q(3);
+            q2q3 = q(3) * q(4);
+            q1q3 = q(2) * q(4);
+            q0q2 = q(1) * q(3);
+            q0q1 = q(1) * q(2);
+
+            r11 = q0q0 + q1q1 - q2q2 - q3q3;
+            r12 = 2*(q1q2 - q0q3);
+            r13 = 2*(q1q3 + q0q2);
+
+            r21 = 2*(q1q2 + q0q3);
+            r22 = q0q0 - q1q1 + q2q2 - q3q3;
+            r23 = 2*(q2q3 - q0q1);
+
+            r31 = 2*(q1q3 - q0q2);
+            r32 = 2*(q2q3 + q0q1);
+            r33 = q0q0 - q1q1 - q2q2 + q3q3;
+            
+            R = [r11 r12 r13;
+                 r21 r22 r23;
+                 r31 r32 r33];
+        end
+        function R = quat_to_rotation_matrix(obj, q)
+            q1q1 = q(2) * q(2);
+            q2q2 = q(3) * q(3);
+            q3q3 = q(4) * q(4);
+            q1q2 = q(2) * q(3);
+            q0q2 = q(1) * q(3);
+            q0q3 = q(1) * q(4);
+            q1q3 = q(2) * q(4);
+            q2q3 = q(3) * q(4);
+            q0q1 = q(1) * q(2);
+
+            r11 = 1.0 - 2.0 * (q2q2 + q3q3);
+            r12 = 2.0 * (q1q2 - q0q3);
+            r13 = 2.0 * (q0q2 + q1q3);
+
+            r21 = 2.0 * (q1q2 + q0q3);
+            r22 = 1.0 - 2.0 * (q1q1 + q3q3);
+            r23 = 2.0 * (q2q3 - q0q1);
+
+            r31 = 2.0 * (q1q3 - q0q2);
+            r32 = 2.0 * (q0q1 + q2q3);
+            r33 = 1.0 - 2.0 * (q1q1 + q2q2);
+            
+            R = [r11 r12 r13;
+                 r21 r22 r23;
+                 r31 r32 r33];
+        end
+
+        function euler_angles = quat_to_euler(obj, q)
+            roll = atan2(2.0*(q(1)*q(2) + q(3)*q(4)), 1.0-2.0*(q(2)*q(2) + q(3)*q(3)));
+            pitch = asin(2.0*(q(1)*q(3) - q(4)*q(2)));
+            yaw = atan2(2.0*(q(1)*q(4) + q(2)*q(3)), 1.0-2.0*(q(3)*q(3) + q(4)*q(4)));
+            
+            roll = rad2deg(roll);
+            pitch = rad2deg(pitch);
+            yaw = rad2deg(yaw);
+            
+            euler_angles = [roll; pitch; yaw];
+        end
+        
+        function ret_obj = complementary_filter(obj, gx, gy, gz, wx, wy, wz, mx, my, mz, dt)
+            %constant
+            q_identy = [1; 0; 0; 0];
+            
+            %normalize accelerometer and magnetometer's reading
+            accelerometer = [gx; gy; gz];
+            accelerometer = accelerometer / norm(accelerometer);
+            magnetometer = [mx; my; mz];
+            magnetometer = magnetometer / norm(magnetometer);
+            
+            %quaternion ingegration
+            half_dt = -0.5 * dt;
+            w = [0; wx; wy; wz];
+            q_dot = obj.quaternion_mult(w, obj.q_last);
+            q_gyro = [obj.q_last(1) + q_dot(1) * half_dt;
+                      obj.q_last(2) + q_dot(2) * half_dt;
+                      obj.q_last(3) + q_dot(3) * half_dt;
+                      obj.q_last(4) + q_dot(4) * half_dt];
+            q_gyro = obj.quat_normalize(q_gyro);
+            
+            %calculate predicted gravity vector
+            conj_q_gyro = obj.quaternion_conj(q_gyro);
+            R_gyro = obj.prepare_body_to_earth_rotation_matrix(conj_q_gyro);
+            g_predict = R_gyro * accelerometer;
+            g_predict = g_predict / norm(g_predict);
+                        
+            %calculate predicted magnetic field vector
+            l_predict = R_gyro * magnetometer; 
+            l_predict = l_predict / norm(l_predict);
+            
+            %calculate delta change of quaternion for fusing gyroscope and aceelerometer
+            weight_accel = 0.005;
+            delta_q_acc = obj.convert_gravity_to_quat(g_predict);
+            delta_q_acc = obj.quat_normalize(delta_q_acc);
+            bar_delta_q_acc = obj.lerp(q_identy, delta_q_acc, weight_accel);
+            bar_delta_q_acc = obj.quat_normalize(bar_delta_q_acc);
+            
+            %calculate delta change of quaternion for fusing gyroscope and magnetometer
+            weight_mag = 0.005;
+            delta_q_mag = obj.convert_magnetic_field_to_quat(l_predict);
+            delta_q_mag = obj.quat_normalize(delta_q_mag);
+            bar_delta_q_mag = obj.lerp(q_identy, delta_q_mag, weight_mag);
+            bar_delta_q_mag = obj.quat_normalize(bar_delta_q_mag);
+            
+            %fuse gyroscope, accelerometer and magnetometer using delte change
+            q_delta_acc_mag = obj.quaternion_mult(bar_delta_q_acc, bar_delta_q_mag);
+            obj.q_estimate = obj.quaternion_mult(q_gyro, q_delta_acc_mag);
+            obj.q_last = obj.q_estimate;
+                        
+            %return the conjugated quaternion since we use the opposite convention compared to the paper
+            %paper: quaternion of earth frame to body-fixed frame
+            %us: quaternion of body-fixed frame to earth frame
+            obj.q_estimate = obj.quaternion_conj(obj.q_estimate);
+            obj.q_estimate = obj.quaternion_mult(obj.q_inclination, obj.q_estimate);
+            euler_angles = obj.quat_to_euler(obj.q_estimate);
+
+            obj.R = obj.quat_to_rotation_matrix(obj.q_estimate);
+            
+            obj.roll = euler_angles(1);
+            obj.pitch = euler_angles(2);
+            obj.yaw = euler_angles(3);
+            
+            ret_obj = obj;
+        end
+    end
+end
---
+++ extended_kalman_filter/main.m
@@ -0,0 +1,184 @@
+format long g
+csv = csvread("gps_imu_compass_barometer_log1.csv");
+
+%ms
+timestamp_ms = csv(:, 1);
+%m/s^2
+accel_lpf_x = csv(:, 2);
+accel_lpf_y = csv(:, 3);
+accel_lpf_z = csv(:, 4);
+%rad/s
+gyro_raw_x = csv(:, 5);
+gyro_raw_y = csv(:, 6);
+gyro_raw_z = csv(:, 7);
+%uT
+mag_raw_x = csv(:, 8);
+mag_raw_y = csv(:, 9);
+mag_raw_z = csv(:, 10);
+%degree
+longitude = csv(:, 11);
+latitude = csv(:, 12);
+%m
+gps_height_msl = csv(:, 13);
+%m/s
+gps_ned_vx = csv(:, 14);
+gps_ned_vy = csv(:, 15);
+gps_ned_vz = csv(:, 16);
+%m
+barometer_height = csv(:, 17);
+%m/s
+barometer_vz = csv(:, 18);
+
+timestamp_s = timestamp_ms .* 0.001;
+[data_num, dummy] = size(timestamp_ms);
+
+%fusion period
+dt = 0.01; %100Hz, 0.01s
+
+ekf = ekf_estimator;
+
+%set home position
+home_longitude = 120.9971619;
+home_latitude = 24.7861614;
+inclination_angle = -23.5;
+ekf = ekf.set_inclination_angle(inclination_angle);
+
+%record datas
+roll = zeros(1, data_num);
+pitch = zeros(1, data_num);
+yaw = zeros(1, data_num);
+%
+gps_enu_x = zeros(1, data_num);
+gps_enu_y = zeros(1, data_num);
+gps_enu_z = zeros(1, data_num);
+%
+fused_enu_x = zeros(1, data_num);
+fused_enu_y = zeros(1, data_num);
+fused_enu_z = zeros(1, data_num);
+%
+fused_enu_vx = zeros(1, data_num);
+fused_enu_vy = zeros(1, data_num);
+fused_eny_vz = zeros(1, data_num);
+%
+%visualization of b1, b2, b3 vectors with quiver3
+b1_visual_sample_cnt = 500;
+quiver_cnt = floor(data_num / b1_visual_sample_cnt);
+quiver_orig_x = zeros(1, quiver_cnt);
+quiver_orig_y = zeros(1, quiver_cnt);
+quiver_orig_z = zeros(1, quiver_cnt);
+quiver_b1_u = zeros(1, quiver_cnt);
+quiver_b1_v = zeros(1, quiver_cnt);
+quiver_b1_w = zeros(1, quiver_cnt);
+quiver_b2_u = zeros(1, quiver_cnt);
+quiver_b2_v = zeros(1, quiver_cnt);
+quiver_b2_w = zeros(1, quiver_cnt);
+quiver_b3_u = zeros(1, quiver_cnt);
+quiver_b3_v = zeros(1, quiver_cnt);
+quiver_b3_w = zeros(1, quiver_cnt);
+j = 1;
+%corrected gravity vector
+gravity_x_arr = zeros(1, data_num);
+gravity_y_arr = zeros(1, data_num);
+gravity_z_arr = zeros(1, data_num);
+gravity_x_arr(1) = accel_lpf_x(1);
+gravity_y_arr(1) = accel_lpf_y(2);
+gravity_z_arr(1) = -accel_lpf_z(3);
+%accelerometer norm and corrected norm
+accelerometer_norm_arr = zeros(1, data_num);
+gravity_norm_arr = zeros(1, data_num);
+
+vel_ned_body = [0; 0; 0];
+                        
+for i = 2: data_num
+    dt = timestamp_s(i) - timestamp_s(i - 1);
+    
+    gravity = [-accel_lpf_x(i);
+               -accel_lpf_y(i);
+               -accel_lpf_z(i)];
+    
+    gravity_x_arr(i) = gravity(1);
+    gravity_y_arr(i) = gravity(2);
+    gravity_z_arr(i) = gravity(3);
+    
+    %attitude estimation
+    ekf = ...
+        ekf.complementary_filter(gravity(1), gravity(2), gravity(3), ...
+                                  gyro_raw_x(i), gyro_raw_y(i), gyro_raw_z(i), ...
+                                  mag_raw_x(i), mag_raw_y(i), mag_raw_z(i), dt);
+    
+    roll(i) = ekf.roll;
+    pitch(i) =ekf.pitch;
+    yaw(i) = ekf.yaw;                   
+end
+
+%%%%%%%%
+% Plot %
+%%%%%%%%
+
+%accelerometer
+figure('Name', 'accelerometer');
+subplot (3, 1, 1);
+plot(timestamp_s, accel_lpf_x);
+title('accelerometer');
+xlabel('time [s]');
+ylabel('ax [m/s^2]');
+subplot (3, 1, 2);
+plot(timestamp_s, accel_lpf_y);
+xlabel('time [s]');
+ylabel('ay [m/s^2]');
+subplot (3, 1, 3);
+plot(timestamp_s, accel_lpf_z);
+xlabel('time [s]');
+ylabel('az [m/s^2]');
+
+%gyroscope
+figure('Name', 'gyroscope');
+subplot (3, 1, 1);
+plot(timestamp_s, rad2deg(gyro_raw_x));
+title('gyroscope');
+xlabel('time [s]');
+ylabel('wx [rad/s]');
+subplot (3, 1, 2);
+plot(timestamp_s, rad2deg(gyro_raw_y));
+xlabel('time [s]');
+ylabel('wy [rad/s]');
+subplot (3, 1, 3);
+plot(timestamp_s, rad2deg(gyro_raw_z));
+xlabel('time [s]');
+ylabel('wz [rad/s]');
+
+%magnatometer
+figure('Name', 'magnetometer');
+subplot (3, 1, 1);
+plot(timestamp_s, mag_raw_x);
+title('magnetometer');
+xlabel('time [s]');
+ylabel('mx [uT]');
+subplot (3, 1, 2);
+plot(timestamp_s, mag_raw_z);
+xlabel('time [s]');
+ylabel('my [uT]');
+subplot (3, 1, 3);
+plot(timestamp_s, mag_raw_z);
+xlabel('time [s]');
+ylabel('mx [uT]');
+
+%estimated roll, pitch and yaw angle
+figure('Name', 'euler angles');
+subplot (3, 1, 1);
+plot(timestamp_s, roll);
+title('euler angles');
+xlabel('time [s]');
+ylabel('roll [deg]');
+subplot (3, 1, 2);
+plot(timestamp_s, pitch);
+xlabel('time [s]');
+ylabel('pitch [deg]');
+subplot (3, 1, 3);
+plot(timestamp_s, yaw);
+xlabel('time [s]');
+ylabel('yaw [deg]');
+
+disp("Press any key to leave");
+pause;
+close all;
